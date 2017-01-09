d635381db86e011ecf25adab62c8b2d1fe4d186c
arshpreet <aashu.wadehra@gmail.com>
2017-01-09 15:22:18 +0530

    v 0.1.3

--- README.md
+++ README.md
@@ -9,7 +9,27 @@ Convert Gif to Static JPG
 
 ``` js
 let imgConvert = require('image-convert').fromURL;
-imgConvert(<url>,<quality[0..100]>,<output_format>,<size{integer}>,function(err,res){
+imgConvert({
+	url:<url>,
+	quality: <quality[0..100]>,
+	output_format:<output_format>,
+	size: <size{integer}>},function(err,res){
+	if(!err)
+		console.log(res);//img buffer
+})
+
+```
+
+## Example
+
+``` js
+let imgConvert = require('image-convert').fromURL;
+imgConvert({
+	url:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Rotating_earth_(large).gif/200px-Rotating_earth_(large).gif",
+	quality: 100,//quality
+	output_format:"jpg",//jpg
+	size: "original"//defualt
+},function(err,res){
 	if(!err)
 		console.log(res);//img buffer
 })
--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const image = require('images');
 const request = require('request').defaults({ encoding: null });
 
 module.exports = {
-  fromURL : ()=>{
-    let quality = 100,output_format="jpg",size=undefined;
-    let callback = arguments[arguments.length-1];
-    let max_allowed_parameters = 5;
-    let min_allowed_parameters = 2;
-    if(arguments.length>max_allowed_parameters || arguments.length < min_allowed_parameters)
-      arguments[arguments.length-1](new Error("Parameters Mismatch"),null);
-    else if(typeof arguments[0] == "function")
-      arguments[arguments.length-1](new Error("URL REQUIRED"),null);
-    let img = arguments[0];
-    if(typeof quality[1] != "function")
-      quality = arguments[1];
-    if(typeof quality[2] != "function")
-      output_format = arguments[2];
-    if(typeof quality[3] != "function")
-      size = arguments[3];
+  fromURL : (options, callback)=>{
+    let {url, quality,output_format,size} = options;
+    if(url == undefined)
+      callback("No URL FOUND",null);
+    if(quality == undefined)
+      quality = 100
+    if(output_format == undefined)
+      output_format = "jpg"
     try{
-      request.get(img, function (err, res, body) {
+      request.get(url, function (err, res, body) {
           var img = image(body);
           var buffer;
-          if(size ==undefined)//original size
+          if(size ==undefined || size == "original")//original size
             buffer = image(body).encode(output_format,{quality:quality});
           else
             buffer = image(body).size(size).encode(output_format,{quality:quality});
